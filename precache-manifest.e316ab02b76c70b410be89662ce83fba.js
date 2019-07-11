1c050295553bb30943bd6ee854dbea8b60feefc9
yusungsamuel <ssyyu@ucdavis.edu>
2019-07-10 20:34:54 -0700

    Updates

---
+++ precache-manifest.e316ab02b76c70b410be89662ce83fba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dd7d6c6d230ff358fe34ddfcbd8649a",
+    "url": "/front-end-dev-test/index.html"
+  },
+  {
+    "revision": "f98e3efe6eef24e40b12",
+    "url": "/front-end-dev-test/static/css/main.5c0c2fc2.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/front-end-dev-test/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "f98e3efe6eef24e40b12",
+    "url": "/front-end-dev-test/static/js/main.94f181cf.chunk.js"
+  },
+  {
+    "revision": "c3f581a01a6a9df68651",
+    "url": "/front-end-dev-test/static/js/runtime~main.3604158a.js"
+  }
+]);
