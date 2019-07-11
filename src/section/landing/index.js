cbc576b33e51c54e5aa977e1c342f79ec82ce2f3
yusungsamuel <ssyyu@ucdavis.edu>
2019-07-10 20:57:48 -0700

    attempt to fix image not showing

--- src/section/landing/index.js
+++ src/section/landing/index.js
@@ -11,7 +11,7 @@ function Landing () {
                     width="12"
                     extraClass="landing"
                 >
-                    <img className="logo" src="/assets/image/logo-white.svg" />
+                    <img className="logo" src="./assets/image/logo-white.svg" alt="logo"/>
                     <div className="text-center welcome-text">
                         <h1 id="welcome">WELCOME TO</h1>
                         <h1 className="company-name">Rooster Grin</h1>
